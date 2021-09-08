65f92ba276851bbb076d76e9fd0b766d1933c2d3
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-09-07 22:00:07 -0400

    variable rules & types

---
+++ variables/Variables.py
@@ -0,0 +1,25 @@
+# ██╗░░░██╗░█████╗░██████╗░██╗░█████╗░██████╗░██╗░░░░░███████╗░██████╗
+# ██║░░░██║██╔══██╗██╔══██╗██║██╔══██╗██╔══██╗██║░░░░░██╔════╝██╔════╝
+# ╚██╗░██╔╝███████║██████╔╝██║███████║██████╦╝██║░░░░░█████╗░░╚█████╗░
+# ░╚████╔╝░██╔══██║██╔══██╗██║██╔══██║██╔══██╗██║░░░░░██╔══╝░░░╚═══██╗
+# ░░╚██╔╝░░██║░░██║██║░░██║██║██║░░██║██████╦╝███████╗███████╗██████╔╝
+# ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝╚═╝░░╚═╝╚═════╝░╚══════╝╚══════╝╚═════╝░
+
+# In Python, variables are indeed case sensitive.
+# For instance, instance and INSTANCE are entirely different variables.
+# Variables must start with a letter or underscore, and can contain
+# numbers. However, they must not start with them.
+
+# Variable Types
+# Variables have different types, similar to those of other programming
+# languages. Types along with an example for each are listed below.
+
+x = 1 # integer (int)
+y = 1.5 # floatval (float)
+name = "Ben" # string (str)
+is_student = True # boolean (bool)
+
+# Note for strings and booleans:
+# Strings can use both "" or '', either serve the same purpose.
+# For booleans, it is important to note that the first letter MUST be capitalized,
+# otherwise it will simply register as a variable and not a boolean.
