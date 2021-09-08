e3e92a5fe25ad812d012c697820c0cad408ea465
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-09-08 11:39:12 -0400

    finished basic math section

---
+++ math/BasicMath.py
@@ -0,0 +1,32 @@
+# Python has quite a few built-in functions for math. Basic addition
+# and subtraction methods can be found below:
+
+# This will output the result of 2 + 1, which equals 3.
+
+a = 2
+b = 1
+result = a + b
+print(result)
+
+# Casting Variables
+# It is possible to cast variables into different types.
+# For example, if x is a variable, we can cast it as a string
+# as follows:
+
+x = 1
+x = str(x)
+
+# Output: "x"
+
+# We can also cast integer types. For example, we can cast a
+# float into an integer like so:
+
+y = 1.583
+y = int(y)
+
+# Conversely, we can cast floats into integers:
+
+z = 2
+z = float(z)
+
+# Output: 2.0
